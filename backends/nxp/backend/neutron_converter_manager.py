601e1c8cbf5f13dbfde86036da631ef89e7f38d3
Lukas Sztefek <lukas.sztefek@nxp.com>
2024-11-05 15:44:47 +0100

    Add NeutronConverterManager that handles TFLite to NeutronGraph-based TFLite conversion

---
+++ backends/nxp/backend/neutron_converter_manager.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2024 NXP
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from neutron_converter_wrapper import neutron_converter
+
+
+class NeutronConverterManager:
+    """
+    Manager for conversion of TFLite model in flatbuffers format into TFLite model that
+    contains NeutronGraph nodes.
+    """
+
+    def convert(self, tflite_model: bytes, target='imxrt700') -> bytes:
+        cctx = neutron_converter.CompilationContext()
+        cctx.targetOpts = neutron_converter.getNeutronTarget(target)
+        model_converted = neutron_converter.convertModel(list(tflite_model), cctx)
+
+        return bytes(model_converted)
---
+++ backends/nxp/tests/test_neutron_converter_manager.py
@@ -0,0 +1,25 @@
+import pytest
+import torch
+
+from executorch import exir
+from executorch.backends.nxp.backend.edge_program_converter import EdgeProgramToIRConverter
+from executorch.backends.nxp.backend.neutron_converter_manager import NeutronConverterManager
+from executorch.backends.nxp.tests.models import Conv2dModule
+
+
+def test_conv2d_neutron_conversion():
+    pytest.importorskip("neutron_converter_wrapper")
+
+    model = Conv2dModule()
+
+    example_input = (torch.ones(1, 4, 32, 32),)
+    exir_program = torch.export.export(model, example_input)
+    edge_program_manager = exir.to_edge(exir_program)
+
+    edge_program_converter = EdgeProgramToIRConverter()
+    tflite_model = edge_program_converter.convert_program(edge_program_manager.exported_program())
+
+    neutron_converter_manager = NeutronConverterManager()
+    neutron_model = neutron_converter_manager.convert(tflite_model)
+
+    assert len(neutron_model), "Produced NeutronGraph-based TFLite model has zero length!"

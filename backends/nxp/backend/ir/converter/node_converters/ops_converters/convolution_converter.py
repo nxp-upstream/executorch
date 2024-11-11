467d0ca9d5eb79732491388af4625eebcf05e218
Lukas Sztefek <lukas.sztefek@nxp.com>
2024-11-11 16:29:14 +0100

    Fix quantized bias tensor conversion for Convolution

--- backends/nxp/backend/ir/converter/node_converters/ops_converters/convolution_converter.py
+++ backends/nxp/backend/ir/converter/node_converters/ops_converters/convolution_converter.py
@@ -10,6 +10,7 @@ from torch.fx import Node
 from executorch.backends.nxp.backend.ir.converter.conversion import common
 from executorch.backends.nxp.backend.ir.converter.conversion.common import try_get_input, OpsList
 from executorch.backends.nxp.backend.ir.converter.node_converter import NodeConverter
+from executorch.backends.nxp.backend.ir.converter.quantization_utils import set_quantization_parameters_to_tensor
 from executorch.backends.nxp.backend.ir.lib.tflite import Padding
 from executorch.backends.nxp.backend.ir.lib.tflite.TensorType import TensorType
 from executorch.backends.nxp.backend.ir.tflite_generator import tflite_model
@@ -18,11 +19,13 @@ from executorch.backends.nxp.backend.ir.tflite_generator.builtin_options import 
 
 class ConvolutionConverter(NodeConverter):
 
-    def _convert_unpadded_2D(self, stride, dilation, t_op) -> OpsList:
-        # Prepare the input and output tensors. To replace them, assign to t_op.tmp_inputs/tmp_outputs directly.
-        output_tensor = t_op.tmp_outputs[0]
-        input_tensor = t_op.tmp_inputs[0]
-        weight_tensor = t_op.tmp_inputs[1]
+    def _convert_2d_conv(self, stride, dilation, t_op: tflite_model.Operator) -> list[tflite_model.Operator]:
+        t_op.builtin_options = conv_2d_options.Conv2D()
+        t_op.builtin_options.padding = Padding.Padding.VALID
+
+        input_tensor: tflite_model.Tensor = t_op.tmp_inputs[0]
+        weight_tensor: tflite_model.Tensor = t_op.tmp_inputs[1]
+        output_tensor: tflite_model.Tensor = t_op.tmp_outputs[0]
 
         if (bias_tensor := try_get_input(t_op, 2)) is None:
             # Operator has no bias. Convolution aten op can omit it, TFLite can't.
@@ -37,6 +40,14 @@ class ConvolutionConverter(NodeConverter):
 
             bias_tensor = self.builder.create_zeros_tensor([output_channels], "zero_bias", bias_type, True)
 
+            # Compute scale and zero point for bias tensor
+            input_scale = np.array(input_tensor.quantization.scale.vector)
+            weight_scale = np.array(weight_tensor.quantization.scale.vector)
+            bias_scale = input_scale * weight_scale
+            bias_zero_point = np.zeros(weight_scale.shape, dtype=np.int64)
+
+            set_quantization_parameters_to_tensor(bias_tensor, bias_scale, bias_zero_point, quantized_dimension=0)
+
         # Assign the operator its TFLite inputs and outputs
         t_op.tmp_inputs = [input_tensor, weight_tensor, bias_tensor]
         t_op.tmp_outputs = [output_tensor]
@@ -47,19 +58,6 @@ class ConvolutionConverter(NodeConverter):
         # Convert the builtin options
         common.assign_2d_strides(t_op.builtin_options, stride)
         common.assign_2d_dilations(t_op.builtin_options, dilation)
-
-        return ops_list
-
-    def _convert_2d_conv(self, stride, dilation, t_op: tflite_model.Operator) -> list[tflite_model.Operator]:
-        t_op.builtin_options = conv_2d_options.Conv2D()
-        t_op.builtin_options.padding = Padding.Padding.VALID
-        ops_list = self._convert_unpadded_2D(stride, dilation, t_op)
-        # TODO Lukas: Try to reuse padding calculation from onnx2tflite
-        # padding, pad_op = conv_utils.build_input_tensor_padding(conv_attributes, t_op, self.builder)
-        # t_op.builtin_options.padding = padding
-
-        # if pad_op is not None:
-        #     conversion_result.ops_list.add_pre(pad_op)
 
         return ops_list.flatten()
 
@@ -81,4 +79,5 @@ class ConvolutionConverter(NodeConverter):
 
         t_op = self._create_tflite_op_with_io_tensors(node)
         ops_to_add = self._convert_2d_conv(stride, dilation, t_op)
+
         self.builder.append_operators(ops_to_add)

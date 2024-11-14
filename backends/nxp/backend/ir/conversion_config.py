63f69f550f6f17ccbbff1cfa9cf71bafdaac580e
Lukas Sztefek <lukas.sztefek@nxp.com>
2024-11-14 14:29:54 +0100

    Propagate IO tensor formats from backend to CompileSpec

--- backends/nxp/backend/ir/conversion_config.py
+++ backends/nxp/backend/ir/conversion_config.py
@@ -15,7 +15,7 @@ class ConversionConfig:
 
         :param args: Optional dictionary with conversion arguments. Unknown arguments are ignored.
         """
-        self.keep_io_format: bool = True
+        self.keep_io_format: bool = False
         self.skip_shape_inference: bool = False
         self.allow_inputs_stripping: bool = True
         self.qdq_aware_conversion: bool = True

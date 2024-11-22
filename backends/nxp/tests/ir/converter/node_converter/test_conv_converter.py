[NO-UPSTREAM] Fix Softmax test and data preprocessors naming

backends/nxp/tests/ir/converter/node_converter/test_conv_converter.py
@@ -1,10 +1,9 @@
 import torch
 
 from executorch import exir
-from executorch.backends.nxp.tests.executors import convert_run_compare
-from executorch.backends.nxp.tests.models import Conv2dModule, LinearModule
-from executorch.backends.nxp.tests.executors import convert_run_compare, ToNCHWPreprocess, ToNHWCPreprocess
+from executorch.backends.nxp.tests.executors import convert_run_compare, ToNHWCPreprocess, ToNCHWPreprocess
 from executorch.backends.nxp.tests.models import Conv2dModule
+from executorch.backends.nxp.tests.models import LinearModule
 
 
 def test_conv2d_conversion():
@@ -17,8 +16,8 @@ def test_conv2d_conversion():
     torch.manual_seed(23)
     input_data = torch.randn((1, 4, 32, 32), dtype=torch.float32).detach().numpy()
 
-    convert_run_compare(edge_program, input_data, tflite_input_preprocess=ToNCHWPreprocess(),
-                        tflite_output_preprocess=ToNHWCPreprocess(), atol=5e-7)
+    convert_run_compare(edge_program, input_data, tflite_input_preprocess=ToNHWCPreprocess(),
+                        tflite_output_preprocess=ToNCHWPreprocess(), atol=5e-7)
 
 
 def test_linear_conversion__with_bias():
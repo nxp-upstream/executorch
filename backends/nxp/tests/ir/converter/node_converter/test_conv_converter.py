ff54223be98c3d122962b4bd77ce2794e5c6cbff
Roman Janik <roman.janik@nxp.com>
2024-11-25 14:55:07 +0100

    [NO UPSTREAM] Split tests for linear converter

--- backends/nxp/tests/ir/converter/node_converter/test_conv_converter.py
+++ backends/nxp/tests/ir/converter/node_converter/test_conv_converter.py
@@ -1,9 +1,8 @@
 import torch
 
 from executorch import exir
-from executorch.backends.nxp.tests.executors import convert_run_compare, ToNHWCPreprocess, ToNCHWPreprocess
+from executorch.backends.nxp.tests.executors import convert_run_compare, ToNCHWPreprocess, ToNHWCPreprocess
 from executorch.backends.nxp.tests.models import Conv2dModule
-from executorch.backends.nxp.tests.models import LinearModule
 
 
 def test_conv2d_conversion():
@@ -18,29 +17,3 @@ def test_conv2d_conversion():
 
     convert_run_compare(edge_program, input_data, tflite_input_preprocess=ToNHWCPreprocess(),
                         tflite_output_preprocess=ToNCHWPreprocess(), atol=5e-7)
-
-
-def test_linear_conversion__with_bias():
-    model = LinearModule(bias=True)
-
-    example_input = (torch.ones(10, 32),)
-    exir_program = torch.export.export(model, example_input)
-    edge_program_manager = exir.to_edge(exir_program)
-
-    torch.manual_seed(23)
-    input_data = torch.randn((10, 32), dtype=torch.float32).detach().numpy()
-
-    convert_run_compare(edge_program_manager.exported_program(), input_data=input_data)
-
-
-def test_linear_conversion__without_bias():
-    model = LinearModule(bias=False)
-
-    example_input = (torch.ones(10, 32),)
-    exir_program = torch.export.export(model, example_input)
-    edge_program_manager = exir.to_edge(exir_program)
-
-    torch.manual_seed(23)
-    input_data = torch.randn((10, 32), dtype=torch.float32).detach().numpy()
-
-    convert_run_compare(edge_program_manager.exported_program(), input_data=input_data)
---
+++ backends/nxp/tests/ir/converter/node_converter/test_linear_converter.py
@@ -0,0 +1,30 @@
+import torch
+
+from executorch import exir
+from executorch.backends.nxp.tests.executors import convert_run_compare
+from executorch.backends.nxp.tests.models import LinearModule
+
+
+def test_linear_conversion__with_bias():
+    model = LinearModule(bias=True)
+
+    example_input = (torch.ones(10, 32),)
+    exir_program = torch.export.export(model, example_input)
+    edge_program_manager = exir.to_edge(exir_program)
+
+    torch.manual_seed(23)
+    input_data = torch.randn((10, 32), dtype=torch.float32).detach().numpy()
+
+    convert_run_compare(edge_program_manager.exported_program(), input_data=input_data)
+
+def test_linear_conversion__without_bias():
+    model = LinearModule(bias=False)
+
+    example_input = (torch.ones(10, 32),)
+    exir_program = torch.export.export(model, example_input)
+    edge_program_manager = exir.to_edge(exir_program)
+
+    torch.manual_seed(23)
+    input_data = torch.randn((10, 32), dtype=torch.float32).detach().numpy()
+
+    convert_run_compare(edge_program_manager.exported_program(), input_data=input_data)

bca639eb7f6b4b95a9319d60173ea0eaf85fe406
Lukas Sztefek <lukas.sztefek@nxp.com>
2024-11-27 14:41:17 +0100

    [NO-UPSTREAM] Refactor tests & add test for Executorch program verification

---
+++ backends/nxp/tests/test_integration.py
@@ -0,0 +1,38 @@
+import os.path
+import pathlib
+
+import pytest
+import torch
+
+from executorch.backends.nxp.tests.executorch_pipeline import to_quantized_executorch_program
+from executorch.backends.nxp.tests.models import ConvFCSoftmaxModule
+from executorch.exir.backend.utils import get_delegation_info
+
+_CURRENT_DIR = pathlib.Path(__file__).parent
+_PROJECT_DIR = _CURRENT_DIR.parent.parent.parent
+
+QUANTIZED_OPS_AOT_LIB_PATH = _PROJECT_DIR / "cmake-build-debug" / "kernels" / "quantized" / "libquantized_ops_aot_lib.so"
+
+
+@pytest.mark.skipif(not os.path.exists(QUANTIZED_OPS_AOT_LIB_PATH), reason="Quant OPS AoT library file not found.")
+def test_conv_fc_softmax__to_executorch_program():
+    torch.ops.load_library(str(QUANTIZED_OPS_AOT_LIB_PATH))
+
+    model = ConvFCSoftmaxModule()
+    input_shape = (1, 4, 5, 5)
+
+    exec_prog = to_quantized_executorch_program(model, input_shape)
+
+    program = exec_prog.exported_program()
+    assert program.graph_module.lowered_module_0, "There is no lowered module with Neutron microcode."
+
+    delegation_info = get_delegation_info(program.graph_module)
+    assert delegation_info.num_delegated_subgraphs == 1
+    assert delegation_info.num_non_delegated_nodes == 5
+    assert delegation_info.num_delegated_nodes == 16
+
+    for node in program.graph.nodes:
+        # Make sure Convolution, AddMM and Softmax are delegated
+        assert "convolution" not in node.name
+        assert "addmm" not in node.name
+        assert "softmax" not in node.name
--- backends/nxp/tests/test_qdq_clustering_conv.py
+++ backends/nxp/tests/test_qdq_clustering_conv.py
@@ -1,34 +1,11 @@
-import torch
-
-from executorch.backends.nxp.neutron_partitioner import NeutronPartitioner
-from executorch.backends.nxp.nxp_backend import generate_neutron_compile_spec
-from executorch.examples.nxp.aot_neutron_compile import post_training_quantize
-from executorch.examples.portable import export_to_edge
-
-
-class Conv2dNoBiasModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.conv = torch.nn.Conv2d(
-            in_channels=4, out_channels=8, kernel_size=3, bias=False, stride=2, dilation=1
-        )
-
-    def forward(self, x):
-        return self.conv(x)
+from executorch.backends.nxp.tests.executorch_pipeline import to_quantized_edge_program
+from executorch.backends.nxp.tests.models import Conv2dModule
 
 
 def test_conv2d_partitioner():
-    calibration_inputs = [(torch.randn((1, 4, 32, 32)),), (torch.randn((1, 4, 32, 32)),)]
-    model = Conv2dNoBiasModule()
-    example_input = (torch.ones(1, 4, 32, 32),)
+    model = Conv2dModule(bias=False)
 
-    exir_program_aten = torch._export.capture_pre_autograd_graph(model, example_input)
-    exir_program_aten_quant = post_training_quantize(exir_program_aten, calibration_inputs)
-    edge_program_manager = export_to_edge(exir_program_aten_quant, example_input)
-
-    partitioner = NeutronPartitioner(generate_neutron_compile_spec("rt700"))
-    edge_program = edge_program_manager.to_backend(partitioner)
+    edge_program = to_quantized_edge_program(model, (1, 4, 32, 32))
 
     # Get subgraph (module) that is delegated to neutron
     lowered_module = edge_program.exported_program().graph_module.lowered_module_0
@@ -47,4 +24,3 @@ def test_conv2d_partitioner():
     assert dq_x_node.meta["cluster"] == "aten_convolution_default_cluster"
     assert conv_node.meta["cluster"] == "aten_convolution_default_cluster"
     assert q_y_node.meta["cluster"] == "aten_convolution_default_cluster"
-    

8070682be46492a4cea03fa1d8960cc17f0dd586
Lukas Sztefek <lukas.sztefek@nxp.com>
2024-12-02 16:47:47 +0100

    [NO-UPSTREAM] Add tests for tensor format in payload

--- backends/nxp/tests/test_neutron_backend.py
+++ backends/nxp/tests/test_neutron_backend.py
@@ -13,7 +13,7 @@ from executorch.backends.nxp.backend.ir.lib.tflite.Model import Model
 from executorch.backends.nxp.tests.executorch_pipeline import to_quantized_edge_program
 from executorch.backends.nxp.tests.executors import TFLiteExecutor, EdgeProgramExecutor, convert_run_compare, \
     ToNHWCPreprocess
-from executorch.backends.nxp.tests.models import Conv2dModule
+from executorch.backends.nxp.tests.models import Conv2dModule, SoftmaxModule
 from executorch.backends.nxp.tests.models import ConvFCSoftmaxModule
 
 
@@ -21,6 +21,28 @@ def test_neutron_backend__single_conv_model():
     edge_program_manager = to_quantized_edge_program(Conv2dModule(bias=False), (1, 4, 32, 32))
     lowered_module = edge_program_manager.exported_program().graph_module.lowered_module_0
     assert len(lowered_module.processed_bytes) != 0  # The Neutron microcode, weights and kernels have been written here
+
+def test_neutron_backend__single_conv_model__payload_header():
+    edge_program_manager = to_quantized_edge_program(Conv2dModule(bias=False), (1, 4, 32, 32))
+    payload = edge_program_manager.exported_program().graph_module.lowered_module_0.processed_bytes
+
+    assert payload[0] == 0x1 # Single input
+    assert payload[1] == 0x1 # Channels last
+    assert payload[2] == 0x1 # Single output
+    assert payload[3] == 0x1 # Channels last
+    assert all(byte == 0x0 for byte in payload[4:16]) # Aligned to 16 bytes
+    assert payload[17] != 0x0 # Followed by non-zero content
+
+def test_neutron_backend__single_softmax_model__payload_header():
+    edge_program_manager = to_quantized_edge_program(SoftmaxModule(1), (1, 64))
+    payload = edge_program_manager.exported_program().graph_module.lowered_module_0.processed_bytes
+
+    assert payload[0] == 0x1 # Single input
+    assert payload[1] == 0x0 # Formatless
+    assert payload[2] == 0x1 # Single output
+    assert payload[3] == 0x0 # Formatless
+    assert all(byte == 0x0 for byte in payload[4:16]) # Aligned to 16 bytes
+    assert payload[17] != 0x0 # Followed by non-zero content
 
 
 def test_lowered_program_and_tflite_output_match__conv2d__no_bias(mocker):

a67e4c0885f68eca63d9b3e8b33b23b132e79b2a
Roman Janik <roman.janik@nxp.com>
2024-12-04 11:39:59 +0100

    [NO UPSTREAM] Refactor tests for constant_pad_nd operator

--- backends/nxp/tests/ir/converter/node_converter/test_constant_pad_nd_converter.py
+++ backends/nxp/tests/ir/converter/node_converter/test_constant_pad_nd_converter.py
@@ -1,16 +1,9 @@
 import pytest
 import torch
-from torch import nn
 
-from executorch import exir
 from executorch.backends.nxp.tests.executors import convert_run_compare, ToNCHWPreprocess, ToNHWCPreprocess
+from executorch.backends.nxp.tests.executorch_pipeline import to_edge_program
 from executorch.backends.nxp.tests.models import ConstantPadNDModule, ConstantPadNDConvModule
-
-
-def _to_edge(model: nn.Module, input_shape):
-    example_input = (torch.ones(input_shape),)
-    exir_program = torch.export.export(model, example_input)
-    return exir.to_edge(exir_program).exported_program()
 
 
 @pytest.mark.parametrize("constant", [0.0, 42., -13.37])
@@ -18,7 +11,7 @@ def test_constant_pad_nd_conversion__specific_constant(constant):
     input_shape = [2, 4, 6, 8]
     paddings = [1, 2, 3, 4]
 
-    edge_program = _to_edge(ConstantPadNDModule(paddings, constant), input_shape)
+    edge_program = to_edge_program(ConstantPadNDModule(paddings, constant), input_shape).exported_program()
 
     torch.manual_seed(23)
     input_data = torch.randn(input_shape, dtype=torch.float32).detach().numpy()
@@ -30,7 +23,7 @@ def test_constant_pad_nd_conversion__default_constant():
     input_shape = [2, 4, 6, 8]
     paddings = [1, 2, 3, 4]
 
-    edge_program = _to_edge(ConstantPadNDModule(paddings), input_shape)
+    edge_program = to_edge_program(ConstantPadNDModule(paddings), input_shape).exported_program()
 
     torch.manual_seed(23)
     input_data = torch.randn(input_shape, dtype=torch.float32).detach().numpy()
@@ -60,7 +53,7 @@ def test_constant_pad_nd_conversion__default_constant():
     pytest.param([1, 2, 3, 4, 5], list(range(10)), id='5D, padding N, C, H, W, D'),
 ])
 def test_constant_pad_nd_conversion__format_less(input_shape, paddings):
-    edge_program = _to_edge(ConstantPadNDModule(paddings), input_shape)
+    edge_program = to_edge_program(ConstantPadNDModule(paddings), input_shape).exported_program()
 
     torch.manual_seed(23)
     input_data = torch.randn(input_shape, dtype=torch.float32).detach().numpy()
@@ -75,7 +68,7 @@ def test_constant_pad_nd_conversion__format_less(input_shape, paddings):
     pytest.param([2, 1, 6, 8], [1, 2, 3, 4, 2, 1, 5, 6], id='4D, padding N, C, H, W'),
 ])
 def test_constant_pad_nd_conversion__channels_first(input_shape, paddings):
-    edge_program = _to_edge(ConstantPadNDConvModule(paddings), input_shape)  # Extra `Conv` after the padding.
+    edge_program = to_edge_program(ConstantPadNDConvModule(paddings), input_shape).exported_program()  # Extra `Conv` after the padding.
 
     torch.manual_seed(23)
     input_data = torch.randn(input_shape, dtype=torch.float32).detach().numpy()

3c60064b52bfd4573e45352b16b297d11eca9773
Roman Janik <roman.janik@nxp.com>
2025-01-23 20:27:42 +0100

    Add NXP examples MLPerf Tiny Keyword Spotting model

---
+++ examples/nxp/models/mlperf_tiny/keyword_spotting/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2025 NXP
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from executorch.examples.nxp.models.mlperf_tiny.keyword_spotting.keyword_spotting import KeywordSpotting
+
+__all__ = ["KeywordSpotting"]
---
+++ examples/nxp/models/mlperf_tiny/keyword_spotting/keyword_spotting.py
@@ -0,0 +1,43 @@
+# Copyright 2025 NXP
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import itertools
+import os
+from typing import Iterator
+
+import torch
+
+from executorch.examples.nxp.models.mlperf_tiny.mlperf_tiny_model import MLPerfTinyModel
+
+
+class KeywordSpotting(MLPerfTinyModel):
+    __input_shape: tuple = (1, 1, 49, 10)
+
+    def __init__(self):
+        super().__init__()
+        self.__dts_path = os.path.join(self._calibration_data_path, "keyword_spotting/calibration_data.pt")
+
+    @property
+    def _dts_path(self):
+        return self.__dts_path
+
+    @property
+    def _input_shape(self):
+        return self.__input_shape
+
+    @staticmethod
+    def _collate_fn(data: torch.Tensor, **kwargs):
+        data, labels = zip(*data)
+        return torch.stack(list(data)).to(memory_format=torch.channels_last), torch.tensor(list(labels))
+
+    def get_calibration_inputs(self, batch_size: int = 1) -> Iterator[tuple[torch.Tensor]]:
+        self._batch_size = batch_size
+        data_loader = self._get_data_loader()
+        get_first = lambda a, b: (a,)
+        return itertools.starmap(get_first, iter(data_loader))
+
+    def get_eager_model(self) -> torch.nn.Module:
+        example_input_shape = self._input_shape[1:]
+        return self._model_manager.get_model("keyword_spotting", input_shape=example_input_shape, num_classes=12)

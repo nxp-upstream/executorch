955bf11ab5caa6ba7177b8b140294c788bc94dd8
Martin Pavella <martin.pavella@nxp.com>
2025-04-24 09:11:18 +0200

    Update licence headers in node converters.

--- backends/nxp/tests/test_edge_passes.py
+++ backends/nxp/tests/test_edge_passes.py
@@ -1,3 +1,9 @@
+# Copyright 2025 NXP
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import numpy as np
 from torch.fx import Node, Graph
 
